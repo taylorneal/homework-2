b7b973ae5750259b8c7e909a958311f0fef257c9
Taylor Neal <taylor.neal@utexas.edu>
2022-03-07 02:08:47 -0600

    Initial setup for problem 4.

--- code/problems-1-4.R
+++ code/problems-1-4.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(rsample)
+library(modelr)
 
 ###
 ### Problem 1 ###
@@ -25,7 +26,7 @@ credit_history$history = str_to_title(credit_history$history)
 ggplot(credit_history, aes(y = default_rate, x = history)) + geom_bar(stat = "identity", fill = "steelblue") + xlab("Credit History") + ylab("Default Rate") + theme_minimal() + ggtitle("Default Probability by Credit History")
 
 # logistic regression model for predicting default probability
-credit_split =  initial_split(credit, prop=0.8)
+credit_split =  initial_split(credit, prop = 0.8)
 credit_train = training(credit_split)
 credit_test  = testing(credit_split)
 
@@ -42,3 +43,27 @@ confusion_out_logit
 ###
 ### Problem 4 ###
 ###
+hotels_dev <- read.csv("https://raw.githubusercontent.com/taylorneal/homework-2/master/data/hotels_dev.csv", header = TRUE)
+hotels_val <- read.csv("https://raw.githubusercontent.com/taylorneal/homework-2/master/data/hotels_val.csv", header = TRUE)
+
+# train / test split
+hotels_dev_split =  initial_split(hotels_dev, prop = 0.8)
+hotels_dev_train = training(hotels_dev_split)
+hotels_dev_test  = testing(hotels_dev_split)
+
+# establish baseline models
+baseline_1 = lm(children ~ market_segment + adults + customer_type + is_repeated_guest, data = hotels_dev_train)
+baseline_2 = lm(children ~ . - arrival_date, data = hotels_dev_train)
+
+# proposed linear model
+hotels_dev_train = hotels_dev_train %>% mutate(two_adults = adults == 2)
+hotels_dev_test = hotels_dev_test %>% mutate(two_adults = adults == 2)
+
+my_model_start = lm(children ~ . - arrival_date - assigned_room_type, data = hotels_dev_train)
+
+lm_step = step(my_model_start, scope = ~ (.)^2)
+
+# compare to baseline
+rmse(baseline_1, hotels_dev_test)
+rmse(baseline_2, hotels_dev_test)
+rmse(my_model, hotels_dev_test)
